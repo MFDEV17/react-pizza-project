0d8595ce3f88f4b522530e0fd29bb519be9fb265
admin <maximsushkin02@gmail.com>
2022-06-30 17:24:05 +0300

    Добавлен парсинг списка из бд

--- src/pages/cartPage/cart_content_list/CartContentlist.js
+++ src/pages/cartPage/cart_content_list/CartContentlist.js
@@ -1,11 +1,19 @@
 import React from "react";
 import CartListItem from "./CartListItem";
+import { useSelector } from "react-redux";
+import { getOrderList } from "../../../redux/cartSlice";
 
-const CartContentList = ({ list = [1, 1, 1, 1, 1, 1, 1, 1, 1] }) => {
+const CartContentList = () => {
+  const orderList = useSelector(getOrderList);
+
   return (
     <div className="content__items">
-      {list.map((item) => (
-        <CartListItem />
+      {orderList.map((i) => (
+        <CartListItem
+          orderPizzaName={i.name}
+          pizzaSize={i.size}
+          pizzaDoughType={i.dough}
+        />
       ))}
     </div>
   );
